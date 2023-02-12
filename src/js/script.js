e70c3db7e140504d42b4b9233d0233ed0138c2f7
WarriorX623 <owenhwtam@gmail.com>
2023-02-12 14:10:04 +0800

    Add projets-section

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,14 @@ gsap.set(".blob", { xPercent: -50, yPercent: -50 });
 const blob = document.querySelector(".blob");
 let mouseX;
 let mouseY;
+
 window.addEventListener("mousemove", e => {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-  gsap.to(blob, 0.8, { x: mouseX, y: mouseY });
+  console.log(window.innerWidth);
+  if (window.innerWidth > 600) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    gsap.to(blob, 0.8, { x: mouseX, y: mouseY });
+  }
 });
 
 const menuButton = document.querySelector(".nav-burger-menu");
@@ -18,3 +22,8 @@ menuButton.addEventListener("click", () => {
 navList.addEventListener("click", () => {
   nav.classList.remove("active");
 });
+import { Application } from "@splinetool/runtime";
+
+const canvas = document.getElementById("canvas3d");
+const app = new Application(canvas);
+app.load("https://prod.spline.design/9SVDrl2aFACVIQ2Z/scene.splinecode");
