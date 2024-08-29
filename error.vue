b2375d3c53cabd4ae93727e87ecebfa1fa34cb6e
Owen Tam <owenhwtam@gmail.com>
2024-08-29 15:00:25 +0800

    Change error page

--- error.vue
+++ error.vue
@@ -1,7 +1,10 @@
 <template>
   <base-section class="container">
     <h2>{{ error.statusCode }}</h2>
-    <h4>Oops! That page doesn't exist! :&#x29;</h4>
+    <h4 v-if="error.statusCode === 404">
+      Oops! That page doesn't exist! :&#x29;
+    </h4>
+    <h4 v-else>Oops! Something went wrong :&#x29;</h4>
     <h5>
       Maybe you can find what you need at our
       <router-link to="/">landing page</router-link>?
@@ -14,6 +17,38 @@ const props = defineProps({
 });
 </script>
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+html {
+  scroll-behavior: smooth;
+}
+:root {
+  --background: #1e2121;
+  --gradient: -webkit-linear-gradient(48deg, #278bff, #28ffed);
+  --primary-color: #278bff;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+div,
+a {
+  font-family: "Poppins", sans-serif;
+}
+a {
+  color: inherit;
+  text-decoration: inherit;
+}
+body {
+  background-color: var(--background);
+  overflow-x: hidden;
+}
 .container {
   text-align: center;
   color: white;
