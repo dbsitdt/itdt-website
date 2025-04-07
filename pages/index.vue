1677078df8b717635b998af12b11734195cadc96
Owen Tam <owenhwtam@gmail.com>
2025-04-07 22:52:52 +0800

    add file

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- <TheFile>https://mega.nz/file/8D8hAACJ#xKjXFmw6ocW181rO_fXXZUXkZglQO4UIYLj9-EkQoqQ</TheFile>-->
     <landing-hero id="hero"></landing-hero>
     <landing-projects id="projects-section"></landing-projects>
     <landing-events id="events"></landing-events>
