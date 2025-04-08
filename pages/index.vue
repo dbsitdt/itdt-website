2426462cfac825246a56b48d568c1db528bd7c74
Owen Tam <owenhwtam@gmail.com>
2025-04-08 22:37:45 +0800

    the file

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <!-- <TheFile>https://mega.nz/file/8D8hAACJ#xKjXFmw6ocW181rO_fXXZUXkZglQO4UIYLj9-EkQoqQ</TheFile>-->
+    <p class="the-file">
+      >https://mega.nz/file/8D8hAACJ#xKjXFmw6ocW181rO_fXXZUXkZglQO4UIYLj9-EkQoqQ
+    </p>
     <landing-hero id="hero"></landing-hero>
     <landing-projects id="projects-section"></landing-projects>
     <landing-events id="events"></landing-events>
@@ -8,3 +10,8 @@
     <landing-contact id="contact-us"></landing-contact>
   </div>
 </template>
+<style scoped>
+.the-file {
+  display: none;
+}
+</style>
